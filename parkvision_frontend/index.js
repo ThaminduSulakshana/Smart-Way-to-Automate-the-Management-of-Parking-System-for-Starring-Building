a2a47798b0a32e3ae0c6d42adc3958d2ac921def
it21006784 <it21006784@my.sliit.lk>
2025-03-18 11:52:33 +0530

    fixed dependency error

--- parkvision_frontend/index.js
+++ parkvision_frontend/index.js
@@ -1,3 +1,5 @@
+import 'react-native-get-random-values'; 
+import 'react-native-polyfill-globals/auto';
 import "@thirdweb-dev/react-native-adapter";
 import "react-native-reanimated";
 import "expo-router/entry";
--- parkvision_frontend/metro.config.js
+++ parkvision_frontend/metro.config.js
@@ -1,14 +1,26 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-/** @type {import('expo/metro-config').MetroConfig} */
+// Get the default configuration
 const config = getDefaultConfig(__dirname);
 
 // Additional thirdweb configuration
-config.resolver.unstable_enablePackageExports = true; // Enable package exports for thirdweb
+config.resolver.unstable_enablePackageExports = true;
 config.resolver.unstable_conditionNames = [
   "react-native",
   "browser",
   "require",
 ];
+
+// Polyfill Node.js modules that are missing in React Native
+config.resolver.extraNodeModules = {
+  stream: require.resolve("stream-browserify"),
+  crypto: require.resolve("react-native-crypto"),
+  assert: require.resolve("assert/"),
+  http: require.resolve("stream-http"),
+  https: require.resolve("https-browserify"),
+  os: require.resolve("os-browserify/browser"),
+  url: require.resolve("url/"),
+  vm: require.resolve("vm-browserify"),
+};
 
 module.exports = config;
